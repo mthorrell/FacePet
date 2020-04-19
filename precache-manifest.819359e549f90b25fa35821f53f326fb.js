0aea1f45961afc92c11c03622499923a26fc5b9a
mthorrell <horrellm87@gmail.com>
2020-04-19 17:22:15 -0500

    Updates

---
+++ precache-manifest.819359e549f90b25fa35821f53f326fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc133d69918eece55d60aea6cb69c1f8",
+    "url": "/Face-Ventura-Pet-Detector-react-app/index.html"
+  },
+  {
+    "revision": "c7bcb5a1d247d1d62607",
+    "url": "/Face-Ventura-Pet-Detector-react-app/static/css/main.2021f3fd.chunk.css"
+  },
+  {
+    "revision": "cb9e615ba4f6674d86d2",
+    "url": "/Face-Ventura-Pet-Detector-react-app/static/js/2.e7bf7c71.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/Face-Ventura-Pet-Detector-react-app/static/js/2.e7bf7c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bcb5a1d247d1d62607",
+    "url": "/Face-Ventura-Pet-Detector-react-app/static/js/main.ded0a97e.chunk.js"
+  },
+  {
+    "revision": "f1d5365979a3247e17a3",
+    "url": "/Face-Ventura-Pet-Detector-react-app/static/js/runtime-main.f1bd7053.js"
+  }
+]);
