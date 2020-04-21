010f491fd775634a9f8f5b9839afd40c278d9b68
mthorrell <horrellm87@gmail.com>
2020-04-21 15:41:47 -0500

    Updates

---
+++ precache-manifest.86c02abb83f5fb25aee1f4ce14db6512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f734ec7523ea50c8a7988313ad8f8882",
+    "url": "/FacePet/index.html"
+  },
+  {
+    "revision": "614d88b3faf5434229f3",
+    "url": "/FacePet/static/css/main.fab3720b.chunk.css"
+  },
+  {
+    "revision": "ca890875ff873420962c",
+    "url": "/FacePet/static/js/2.9b7b3c82.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/FacePet/static/js/2.9b7b3c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614d88b3faf5434229f3",
+    "url": "/FacePet/static/js/main.fa8e7a51.chunk.js"
+  },
+  {
+    "revision": "ec1aa6ddbdb785232c90",
+    "url": "/FacePet/static/js/runtime-main.6ee8f81b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FacePet/precache-manifest.7313552a4c5f5acdc208ff37fad8d562.js"
+  "/FacePet/precache-manifest.86c02abb83f5fb25aee1f4ce14db6512.js"
 );
 
 self.addEventListener('message', (event) => {
